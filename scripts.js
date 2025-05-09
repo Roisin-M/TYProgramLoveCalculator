2ee3489aed4f0de9eb71f33f1cc82c5aa39d50dc
Roisin Muldoon <rmuldoon@beyond.com>
2025-05-09 12:10:12 +0100

    cardelement

--- scripts.js
+++ scripts.js
@@ -1,26 +1,51 @@
+let selectedCrush = '';
+let selectedCard = null;
+
+function selectCrush(name, cardElement) {
+  selectedCrush = name;
+
+  // Visually mark selected card
+  if (selectedCard) selectedCard.classList.remove('selected');
+  cardElement.classList.add('selected');
+  selectedCard = cardElement;
+}
+
 function calculateLoveMatch() {
-    // We set two names here, which we will use to get the love match between these two people
-    // We set two names here, which we will use to get the love match between these two people
-    var name1 = 'John Doe';
-    var name2 = 'Jane Doe';
+  const name1 = document.getElementById('userName').value.trim();
+  const name2 = selectedCrush;
 
-     // We need to set a few things here, to make sure our API works. Don't change these options, or you'll break it!
-    const options = {
-        method: 'GET',
-        headers: {
-           'x-api-key': '1f0d0629-463a-4850-887e-00149b7addd8'
-        }
-    };
+  if (!name1 || !name2) {
+    document.getElementById('result').innerText = 'Please enter your name and select a crush!';
+    return;
+  }
 
-       // This is where we fetch the data from our API.
-       fetch('https://api.apiverve.com/v1/lovecalculator?name1=' + encodeURI(name1) + '&name2=' + encodeURI(name2), options)
-        .then(response => response.json())
-        .then(response => {
-        console.log(response);
-        console.log(response.data);
-        // This is where you populate the html elements and
-        // element attributes with values from the result;
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-api-key': '1f0d0629-463a-4850-887e-00149b7addd8'
+    }
+  };
 
-        })
-        .catch(err => console.error(err));
+  fetch(`https://api.apiverve.com/v1/lovecalculator?name1=${encodeURIComponent(name1)}&name2=${encodeURIComponent(name2)}`, options)
+    .then(response => response.json())
+    .then(response => {
+      console.log(response);
+      const data = response.data;
+      console.log(response.data);
+      if (data) {
+        document.getElementById('result').innerHTML = `
+          💞 ${data.name1} + ${data.name2} = <strong>${data.lovePercentage}%</strong><br>
+          📝 "${data.response}"
+        `;
+      } else {
+        document.getElementById('result').innerText = 'Sorry, something went wrong.';
+      }
+    })
+    .catch(err => {
+      console.error(err);
+      document.getElementById('result').innerText = 'Error contacting the love calculator.';
+    });
+    function determineMessage(){
+        
+    }
 }
